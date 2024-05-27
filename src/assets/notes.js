68e4d0f3ab906c0d0d73f8561b62758a575ef004
lucky dogra <lucky.dogra@itechnolabs.tech>
2024-05-27 15:18:45 +0530

    optn_composit  commit

---
+++ src/assets/notes.js
@@ -0,0 +1,1 @@
+//components and its types
---
+++ src/components/options.vue
@@ -0,0 +1,31 @@
+// options api
+<script>
+export default {
+    data() {
+        return {
+            count:12
+        }
+    },
+    methods: {
+       increment(){
+        this.count++
+       } 
+    },
+    mounted() {
+        console.log(">>>>>>>>>>>>>>>");
+    },
+}
+</script>
+
+
+// compositions api 
+<script>
+import {ref,onMounted } from "vue";
+const count =ref(12);
+function increment(params) {
+    count.value++
+}
+onMounted(() => {
+    console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
+}),
+</script>
