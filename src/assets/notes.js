63bf36c0b611a7ea0229af62c6fa4c5a7d91a3dc
lucky dogra <lucky.dogra@itechnolabs.tech>
2024-05-27 16:50:16 +0530

    api or coding styles and components

--- src/assets/notes.js
+++ src/assets/notes.js
@@ -1,1 +1,12 @@
+// api or coding styles in vue.js are optional and compositions
+// option types were practiced before vue3 and follow oops concepts 
+
+
 //components and its types
+//sfc- single file component that contains js, css , html block.
+//these components contains manly three langue i.e template block, script block and styling block.
+// use src for importing seperate styling, script or template block.
+// i.e.
+//  <template src = "./abc.html">
+
+//  </template>
