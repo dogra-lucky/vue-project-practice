26cf197c2bd8898f40c3638514eb049e05115fa4
lucky dogra <lucky.dogra@itechnolabs.tech>
2024-05-28 14:53:34 +0530

    vue components types

--- src/assets/notes.js
+++ src/assets/notes.js
@@ -10,3 +10,34 @@
 //  <template src = "./abc.html">
 
 //  </template>
+
+
+
+
+
+//chapter 2 (vue components and its types)
+
+// local components and global components 
+
+//local components are imported in script block of component and used in parent components 
+// i.e 
+// <script > import compA from "./components/compA" </script>
+// <template> <compA/>  </template>
+
+
+// global components are imported in main.js file and can be used in other components without import
+// i.e
+// code in main.js file 
+// import compA from "./components/compA"
+// const app = createApp({});
+// app.component('compA', compA); // Register globally
+// app.mount('#app');
+
+
+// chapter3 html tag attribute binding
+// 
+// <img v-bind:attr_name="attr_value">  </img>
+// <img :attr_name="attr_value">  </img>
+
+
+
